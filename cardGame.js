67024f31b1db83c22eaa3c07b56a6f9b92dd40b2
Steve White <stephenkrohnwhite@gmail.com>
2018-10-09 15:56:45 -0500

    added functionality to shuffle, deal

--- cardGame.js
+++ cardGame.js
@@ -1,28 +1,83 @@
 "use strict"
 var deck = [];
-function Card(suitName, cardValue) {
-	this.suit = suitName;
-	this.cardValue = cardValue;
-}	
-
+var playersDeck = [];
 var cardSequence = ["ace","2","3","4","5","6","7","8","9","10","jack","queen","king"];
 function deckBuilder(deckArray) {
 	let suits = ["hearts", "diamonds", "spades", "clovers"]
 	for(let i = 0; i < suits.length; i++) {
 		let deckStack = buildSuit(suits[i]);
-		deckArray += deckStack;
+		for(let j = 0; j < deckStack.length; j++) {
+			deckArray.push(deckStack[j]);
+		}
 	}
-	for(let i = 0; i<deck.Length; i++) {
+	for(let i = 0; i<deck.length; i++) {
 		console.log(deckArray[i].cardValue);
 	}
+	return deckArray;
 }
 function buildSuit(suitName) {
 	let suitArray = [];
 	for(let i = 0; i < 13; i++) {
-		suitArray[i] = new Card(suitName, cardSequence[i]);
+		let card = {
+			suit: suitName,
+			cardValue: cardSequence[i]
+		};
+		suitArray.push(card);
 	}
 	return suitArray;
 }
+function dealDeck(totalPlayers, deck, players) {
+	let pile = deck;
+	let cardsToDeal = getCardsToDeal(deck, totalPlayers);
+	while(pile.length > 0) {
+		for(let i = 0; i < totalPlayers; i++) {
+				players[i].pile.push(pile[i]);
+				cardsToDeal--;
+				pile.splice(0,1);
+		}
+	}
+}
+function getCardsToDeal(deck, totalPlayers) {
+	let remainder = deck.length % totalPlayers;
+	let numberToDeal = deck.length - remainder;
+	return numberToDeal;
+}
+function getPlayers() {
+	let playerNumber = prompt("Please enter number of players");
+	if(validateUserInput(playerNumber) == true) {
+		return playerNumber;
+	}
+	else {
+		return getPlayers();
+	}
+}
+function validateUserInput(input) {
+	let parseValue = parseInt(input);
+	if(parseValue > 52 || parseValue == NaN) {
+		alert("Not a valid entry");
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+function playerBuilder(totalPlayers) {
+	let players = [];
+	for(let i = 0; i < totalPlayers; i++) {
+		let player = {
+			name: "player" + (i+1).toString(),
+			pile: []
+		}
+		players.push(player);
+	}
+	return players;
+}
+
 function runProgram() {
-	deckBuilder(deck);
+	let numberOfPlayers = getPlayers();
+	deck = deckBuilder(deck);
+	alert("Deck is being shuffled");
+	deck.sort(function(a,b){return .5 - Math.random()});
+	playersDeck = playerBuilder(numberOfPlayers);
+	dealDeck(numberOfPlayers, deck, playersDeck);
 }
